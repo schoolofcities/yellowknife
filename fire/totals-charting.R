c011a104170fe19178765532ca320b682cdfed3c
Jeff Allen <jeffallen89@gmail.com>
2024-08-26 09:53:06 -0400

    update burn total map

--- fire/totals-charting.R
+++ fire/totals-charting.R
@@ -2,5 +2,12 @@ library(tidyverse)
 
 df <- read.csv("fire-area/fire-totals-canada-by-year.csv")
 
+df <- head(df, 25)
+
 ggplot() +
-  geom_bar(aes(x = factor(df$YEAR), y = df$CANADA), stat = "identity")
+  geom_bar(aes(x = factor(df$YEAR), y = df$CANADA, fill = df$CANADA), stat = "identity") +
+  labs(x = "Year", y = "Area Burned") +
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1.6, vjust = 2.6)) + 
+  scale_fill_gradient(low = "#F1C500", high = "#DC4633")
+  
